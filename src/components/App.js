6820d8cdf6ac9bdc080426f7222a7eacbad1a0ad
Aristóteles Núñez <aristoteles.nunez@gmail.com>
2020-02-13 13:05:18 -0600

    refactor(app): installed material-ui
    Installed and configured material-ui to use in the project

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import Button from '@material-ui/core/Button';
 
 function App() {
   return (
     <div>
       Would you rather?
+      <Button variant="contained" color="primary">
+        Hello World
+      </Button>
     </div>
   );
 }
